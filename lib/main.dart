da689a00b573b1a69dc9655880436f18873b5c73
owen <owen@cetustek.com.tw>
2021-04-27 09:17:24 +0800

    Fiutter Hello~World!

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('First APP')),
+      body: HomePage(),
+    ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello World'),
+    );
+  }
+}
